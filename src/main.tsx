26de35e27f41ce11a45f1c59654945deb5fb50bc
novaframerealty <novaframerealty@gmail.com>
2025-09-14 18:02:14 -0500

    feat: add themed hero gradient and typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './GlobalStyle'
+import theme from './theme'
 import './styles/globals.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
